c9c2aa002462ab8e9c79f88824fed760e12faeae
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-04 20:59:33 +0100

    5-beginning_and_end.rb

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
+# exactly matching a string that starts with h ends with n
+# and can have any single character in between
+puts ARGV[0].scan(/^h.n$/).join
