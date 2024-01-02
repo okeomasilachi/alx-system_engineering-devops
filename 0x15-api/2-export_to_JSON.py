4d3cf500932ccb646e182c097862d58483f0162f
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-02 19:28:33 +0100

    Using what you did in the task #0, extend your Python script to export data in the JSON format.
    Requirements:
    
    Records all tasks that are owned by this employee
    Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
    File name must be: USER_ID.json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+
+"""
+using a REST-API, for a given employee ID, returns
+information about his/her TODO list progress.
+"""
+
+from requests import get
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    if len(argv) >= 2:
+        un = get(f"https://jsonplaceholder.typicode.com/users"
+                 f"/{argv[1]}")
+        ut = get(f"https://jsonplaceholder.typicode.com/users"
+                 f"/{argv[1]}/todos")
+        if un.status_code == 200:
+            UNAME = un.json().get("username")
+            data = ut.json()
+            with open(f"{data[0].get('userId')}.json", "w",
+                      encoding="utf-8") as file:
+                main = {}
+                ret_list = []
+                for i in data:
+                    ret_list.append({
+                        "task": i.get('title'),
+                        "completed": i.get('completed'),
+                        "username": UNAME
+                        })
+                main[i.get('userId')] = ret_list
+                file.write(json.dumps(main))
