831a1c253e5fab38b9adea8e35e725e9534323a6
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-02 20:53:34 +0100

    Using what you did in the task #0, extend your Python script to export data in the JSON format.
    Requirements:
    
    Records all tasks from all employees
    Format must be: { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ], "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
    File name must be: todo_all_employees.json

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+
+"""
+using a REST-API, for a given employee ID, returns
+information about his/her TODO list progress.
+"""
+
+from requests import get
+import json
+
+
+if __name__ == "__main__":
+    un = get(f"https://jsonplaceholder.typicode.com/users")
+    ut = get(f"https://jsonplaceholder.typicode.com/todos")
+    main = {}
+    ret_list = [
+        {
+            "username": user.get("username"),
+            "task": task.get("title"),
+            "completed": task.get("completed")
+        }
+        for user in un.json()
+        for task in ut.json()
+        if user.get("id") == task.get("userId")
+    ]
+
+    for user in un.json():
+        main[user.get("id")] = [task for task in ret_list
+                                if task["username"] == user.get("username")
+                                ]
+
+    with open("todo_all_employees.json", "w", encoding="utf-8") as file:
+        file.write(json.dumps(main))
