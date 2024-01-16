44afc848bce3e3078b41f0683dcb718d41f3b532
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-16 23:01:30 +0100

    Fix user agent in API request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 
 """
-Module to query the Reddit API and return the number
+Module to query the Reddit API and returns the number
 of subscribers
 """
 
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number
-    of subscribers
+    Queries the Reddit API and returns the number of subscribers
     for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}
-    response = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-        )
+    headers = {"User-Agent": "okeoma"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
